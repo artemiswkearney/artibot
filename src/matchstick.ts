1282a156687c2adc567d9395820803f8e1137c6d
Arti Kearney <artemis.wk@gmail.com>
2020-01-14 00:10:56 -0800

    Document Matchstick

--- src/matchstick.ts
+++ src/matchstick.ts
@@ -1,10 +1,18 @@
+/* Matchstick, a one-file library for RegExp-based pattern matching */
 type MatchPair = [RegExp, (_: RegExpMatchArray, ...args: string[]) => any];
 
+/* A curried function.
+ * First application: Takes any number of [RegExp, callback] pairs.
+ * Second application: Takes a string,
+ * Checks the string against each RegExp, then calls the callback of the first matching one.
+ * The first argument to the callback is the returned RegExpMatchArray.
+ * All unnamed capture groups' contents are passed in order as the remaining arguments.
+ */
 const match = (...pairs: MatchPair[]) => (str : string) => {
 	for (let [rx, f] of pairs) {
 		if (!rx.test(str)) continue;
 		let match : RegExpMatchArray = rx.exec(str)!;
-		f(match, ...match.slice(1));
+		return f(match, ...match.slice(1));
 	}
 }
 
